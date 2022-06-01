8d31d34e0efcaebb271542e365707efe6054a824
asadullah696 <mr.asadullah19@gmail.com>
2022-06-02 00:32:50 +0500

    Code for Comparison Operators

--- app.js
+++ app.js
@@ -1,76 +1,98 @@
 
-// // Alert Box
-// // alert("This is an alert Box!");
+// Alert Box
+// alert("This is an alert Box!");
 
-// // Variable for strings
-// var stringVariable = "Variable for Strings.";
-// var resultStringVariable = "This line is to justify " + stringVariable;
-// console.log(stringVariable);
-// console.log(resultStringVariable);
-// window.document.write(resultStringVariable);
+// Variable for strings
+var stringVariable = "Variable for Strings.";
+var resultStringVariable = "This line is to justify " + stringVariable;
+console.log(stringVariable);
+console.log(resultStringVariable);
+window.document.write(resultStringVariable);
 
 
-// // Variable for nummbers
-// var numberVariable = 209 ;
-// var resultNumberVariable = " This line is to justify Numbers Variable to add 100  in the number which is defined for Number as a variable, " + (100 + numberVariable) + ". ";
-// console.log(numberVariable);
-// console.log(resultNumberVariable);
-// window.document.write(resultNumberVariable);
+// Variable for nummbers
+var numberVariable = 209 ;
+var resultNumberVariable = " This line is to justify Numbers Variable to add 100  in the number which is defined for Number as a variable, " + (100 + numberVariable) + ". ";
+console.log(numberVariable);
+console.log(resultNumberVariable);
+window.document.write(resultNumberVariable);
 
-// // Variable Names Legal & Illegal
-// var legalVariable = " All Variable are legal that are in camel case, and can't contain the special character except $ and _ sign.";
-// var illegalVariable = " All Variable are illegal that having not camel case, and special letter in it, ";
-// console.log(legalVariable);
-// console.log(illegalVariable);
-// window.document.write(legalVariable);
-// window.document.write(illegalVariable);
+// Variable Names Legal & Illegal
+var legalVariable = " All Variable are legal that are in camel case, and can't contain the special character except $ and _ sign.";
+var illegalVariable = " All Variable are illegal that having not camel case, and special letter in it, ";
+console.log(legalVariable);
+console.log(illegalVariable);
+window.document.write(legalVariable);
+window.document.write(illegalVariable);
 
-// // Math expression (Familiar Operation)
-// // Adding
-// var mathExpression = Number(prompt("Write here any number to add in 100"));
-// var resultMathExpression = "This line is to practice the Familiar Operation of math expression " + (100 + mathExpression ) +". " ;
-// console.log(mathExpression);
-// console.log(resultMathExpression);
-// window.document.write(resultMathExpression)
+// Math expression (Familiar Operation)
+// Adding
+var mathExpression = Number(prompt("Write here any number to add in 100"));
+var resultMathExpression = "This line is to practice the Familiar Operation of math expression " + (100 + mathExpression ) +". " ;
+console.log(mathExpression);
+console.log(resultMathExpression);
+window.document.write(resultMathExpression)
 
-// // Subtracting
-// var mathExpression1 = Number(prompt("Write here any number to Subtract in 500"));
-// var resultMathExpression1 = "This line is to practice the Familiar Operation of math expression " + (500 - mathExpression1) + "." ;
-// console.log(mathExpression1);
-// console.log(resultMathExpression1);
-// window.document.write(resultMathExpression1);
+// Subtracting
+var mathExpression1 = Number(prompt("Write here any number to Subtract in 500"));
+var resultMathExpression1 = "This line is to practice the Familiar Operation of math expression " + (500 - mathExpression1) + "." ;
+console.log(mathExpression1);
+console.log(resultMathExpression1);
+window.document.write(resultMathExpression1);
 
-// // Multiply
-// var mathExpression2 = Number(prompt("Write here any number to Multiply by 07"));
-// var resultMathExpression2 = " This line is to practice the Familiar Operation of math expression " + (7 * mathExpression2) + "." ;
-// console.log(mathExpression2);
-// console.log(resultMathExpression2);
-// window.document.write(resultMathExpression2);
+// Multiply
+var mathExpression2 = Number(prompt("Write here any number to Multiply by 07"));
+var resultMathExpression2 = " This line is to practice the Familiar Operation of math expression " + (7 * mathExpression2) + "." ;
+console.log(mathExpression2);
+console.log(resultMathExpression2);
+window.document.write(resultMathExpression2);
 
-// // Divide
-// var mathExpression3 = Number(prompt("Write here any number to Divide by 500"));
-// var resultMathExpression3 = " This line is to practice the Familiar Operation of math expression " + (500 / mathExpression3) + "." ;
-// console.log(mathExpression3);
-// console.log(resultMathExpression3);
-// window.document.write(resultMathExpression3);
+// Divide
+var mathExpression3 = Number(prompt("Write here any number to Divide by 500"));
+var resultMathExpression3 = " This line is to practice the Familiar Operation of math expression " + (500 / mathExpression3) + "." ;
+console.log(mathExpression3);
+console.log(resultMathExpression3);
+window.document.write(resultMathExpression3);
 
-// // Math Expression (Unfamiliar Operation)
-// // Example 
-// var unfamiliarExpression = Number(prompt("Please select the number of shirts, You want to purchase. "));
-// var unfamiliarExpression1 = Number(prompt("Please select the numbers of pents, You wants to purchase. "));
-// var resultUnfamiliarExpression = unfamiliarExpression + unfamiliarExpression1 + ". " ;
-// console.log(unfamiliarExpression);
-// console.log(unfamiliarExpression1);
-// window.document.write(resultUnfamiliarExpression);
+// Math Expression (Unfamiliar Operation)
+// Example 
+var unfamiliarExpression = Number(prompt("Please select the number of shirts, You want to purchase. "));
+var unfamiliarExpression1 = Number(prompt("Please select the numbers of pents, You wants to purchase. "));
+var resultUnfamiliarExpression = unfamiliarExpression + unfamiliarExpression1 + ". " ;
+console.log(unfamiliarExpression);
+console.log(unfamiliarExpression1);
+window.document.write(resultUnfamiliarExpression);
 
 // If Statement
 var cityState = prompt("Write any City of Pakistan to check, that are in top 5. ");
 var cities = ["Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Gujranwala"];
 if(cities === cityState){
     alert("This city are in top 5 Cities. ");
-}
+};
 else{
     alert("This city are not in top 5 Cities. ");
+};
+
+// Comparison Operator
+var comparisonOperatora = Number(prompt("Write any number to suppose number as 'a',"));
+var comparisonOperatorb = Number(prompt("Write any number to suppose number as the value of 'b',"));
+var comparisonOperatorResultab = comparisonOperatora + comparisonOperatorb;
+console.log(comparisonOperatora);
+console.log(comparisonOperatorb);
+console.log(comparisonOperatorResultab);
+window.document.write(comparisonOperatorResultab);
+var comparisonOperatorx = Number(prompt("Write any number to suppose number as 'x', "));
+var comparisonOperatory = Number(prompt("Write any number to suppose number as 'y', "));
+var comparisonOperatorResultxy = comparisonOperatorx * comparisonOperatory;
+console.log(comparisonOperatorx);
+console.log(comparisonOperatory);
+console.log(comparisonOperatorResultxy);
+window.document.write(comparisonOperatorResultxy);
+if(comparisonOperatorResultab === comparisonOperatorResultxy){
+    alert("The Value of a+b is equal to x*y.");
+}
+else{
+    alert("The value of a+b is not equal to x*y.");
 }
 
 
