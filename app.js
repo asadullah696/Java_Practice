0f69edf93d2431f7640e9d961cd76ceb2557901b
asadullah696 <mr.asadullah19@gmail.com>
2022-06-01 23:26:56 +0500

    Code to practice for Variables for Numbers and Strings

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+
+// Alert Box
 // alert("This is an alert Box!");
+
+// Variable for strings
+var stringVariable = "Variable for Strings.";
+var resultStringVariable = "This line is to justify " + stringVariable;
+console.log(stringVariable);
+console.log(resultStringVariable);
+window.document.write(resultStringVariable);
+
+
+// Variable for nummbers
+var numberVariable = 209;
+var resultNumberVariable = "This line is to justify Numbers Variable to add 100  in the number which is defined for Number as a variable, " + (100 + numberVariable);
+console.log(numberVariable);
+console.log(resultNumberVariable);
+window.document.write(resultNumberVariable);
 
