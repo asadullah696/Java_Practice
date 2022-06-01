2f6760456e28d18ff42dfe4aa3738dfa74bfd38d
asadullah696 <mr.asadullah19@gmail.com>
2022-06-01 23:52:07 +0500

    Math Expression(Familiar Operations)

--- app.js
+++ app.js
@@ -11,17 +11,47 @@ window.document.write(resultStringVariable);
 
 
 // Variable for nummbers
-var numberVariable = 209;
-var resultNumberVariable = "This line is to justify Numbers Variable to add 100  in the number which is defined for Number as a variable, " + (100 + numberVariable);
+var numberVariable = 209 ;
+var resultNumberVariable = " This line is to justify Numbers Variable to add 100  in the number which is defined for Number as a variable, " + (100 + numberVariable) + ". ";
 console.log(numberVariable);
 console.log(resultNumberVariable);
 window.document.write(resultNumberVariable);
 
 // Variable Names Legal & Illegal
-var legalVariable = "All Variable are legal that are in camel case, and can't contain the special character except $ and _ sign.";
-var illegalVariable = "All Variable are illegal that having not camel case, and special letter in it, ";
+var legalVariable = " All Variable are legal that are in camel case, and can't contain the special character except $ and _ sign.";
+var illegalVariable = " All Variable are illegal that having not camel case, and special letter in it, ";
 console.log(legalVariable);
 console.log(illegalVariable);
 window.document.write(legalVariable);
 window.document.write(illegalVariable);
+
+// Math expression (Familiar Operation)
+// Adding
+var mathExpression = Number(prompt("Write here any number to add in 100"));
+var resultMathExpression = "This line is to practice the Familiar Operation of math expression " + (100 + mathExpression ) +". " ;
+console.log(mathExpression);
+console.log(resultMathExpression);
+window.document.write(resultMathExpression)
+
+// Subtracting
+var mathExpression1 = Number(prompt("Write here any number to Subtract in 500"));
+var resultMathExpression1 = "This line is to practice the Familiar Operation of math expression " + (500 - mathExpression1) + "." ;
+console.log(mathExpression1);
+console.log(resultMathExpression1);
+window.document.write(resultMathExpression1);
+
+// Multiply
+var mathExpression2 = Number(prompt("Write here any number to Multiply by 07"));
+var resultMathExpression2 = " This line is to practice the Familiar Operation of math expression " + (7 * mathExpression2) + "." ;
+console.log(mathExpression2);
+console.log(resultMathExpression2);
+window.document.write(resultMathExpression2);
+
+// Divide
+var mathExpression3 = Number(prompt("Write here any number to Divide by 500"));
+var resultMathExpression3 = " This line is to practice the Familiar Operation of math expression " + (500 / mathExpression3) + "." ;
+console.log(mathExpression3);
+console.log(resultMathExpression3);
+window.document.write(resultMathExpression3);
+
 
